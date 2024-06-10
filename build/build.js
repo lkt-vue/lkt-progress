06511b53ec7c7e8402246eb1e8350049374c8495
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2024-06-10 17:56:23 +0200

    Percentage selectors

--- build/build.js
+++ build/build.js
@@ -17,13 +17,33 @@ const _sfc_main = /* @__PURE__ */ defineComponent({
       let r = [];
       if (props.palette)
         r.push(`lkt-progress--${props.palette}`);
+      if (computedPercentage.value >= 10)
+        r.push("lkt-progress--fill-10");
+      if (computedPercentage.value >= 20)
+        r.push("lkt-progress--fill-20");
+      if (computedPercentage.value >= 30)
+        r.push("lkt-progress--fill-30");
+      if (computedPercentage.value >= 40)
+        r.push("lkt-progress--fill-40");
+      if (computedPercentage.value >= 50)
+        r.push("lkt-progress--fill-50");
+      if (computedPercentage.value >= 60)
+        r.push("lkt-progress--fill-60");
+      if (computedPercentage.value >= 70)
+        r.push("lkt-progress--fill-70");
+      if (computedPercentage.value >= 80)
+        r.push("lkt-progress--fill-80");
+      if (computedPercentage.value >= 90)
+        r.push("lkt-progress--fill-90");
+      if (computedPercentage.value >= 100)
+        r.push("lkt-progress--fill-100");
       return r.join(" ");
     }), computedPercentage = computed(() => {
       if (props.modelValue > 100)
         return 100;
       if (props.modelValue < 0)
         return 0;
-      return Number(props.modelValue).toFixed(2);
+      return Number(Number(props.modelValue).toFixed(2));
     }), computedVisiblePercentage = computed(() => {
       return Number(props.modelValue).toFixed(2);
     }), progressBarStyles = computed(() => {
